faa1246de3740bade21e985f4a711534c4e713a2
sgstanino <sgstanino@gmail.com>
2026-04-16 19:56:11 +0900

    Update ruffleimport.js

--- cdr/sutanbai/ruffleimport.js
+++ cdr/sutanbai/ruffleimport.js
@@ -1,4 +1,10 @@
-document.write('<script src = "https://unpkg.com/@ruffle-rs/ruffle"> </ script>');
+(function() {
+  var s = document.createElement('script');
+  s.src = 'https://unpkg.com/@ruffle-rs/ruffle';
+  document.head.appendChild(s);
+})();
+
+//document.write('<script src = "https://unpkg.com/@ruffle-rs/ruffle"> </ script>');
 
 //document.write('<script src="../ruffle/ruffle.js"></script>');
 
