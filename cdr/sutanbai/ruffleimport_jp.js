df6d230ff2e7d822737e59e4ab3bd58a8fde4571
sgstanino <sgstanino@gmail.com>
2026-04-16 22:36:19 +0900

    Update ruffleimport_jp.js

--- cdr/sutanbai/ruffleimport_jp.js
+++ cdr/sutanbai/ruffleimport_jp.js
@@ -4,10 +4,17 @@
 //これは、海外ruffleサイトが自動更新してくれるサイト
 //document.write('<script src="https://unpkg.com/@ruffle-rs/ruffle"></script>');
 //
-//これは、緑SM64さんが作ってくれた日本語対応ruffleへのリンク・・日本語対応ruffleについては、時々、エミュレーターのファイルを更新しないといけない。・・自動のリンクは無い。
-document.write('<script src="https://sgstanino.github.io/sgsstudy/cdr/sutanbai/ruffle_jp/ruffle.js"></script>');
-
+//★これは、緑SM64さんが作ってくれた日本語対応ruffleへのリンク・・日本語対応ruffleについては、時々、エミュレーターのファイルを更新しないといけない。・・自動のリンクは無い。
+//document.write('<script //src="https://sgstanino.github.io/sgsstudy/cdr/sutanbai/ruffle_jp/ruffle.js"></script>');
+//ここまで★
 //document.write('<script src="../sutanbai/ruffle_jp/ruffle.js"></script>');
 //
 //document.write('<script src="../ruffle/ruffle.js"></script>');
+//
+//Claudeのアドバイスによるもの⇓
+(function() {
+  var s = document.createElement('script');
+  s.src = 'https://sgstanino.github.io/sgsstudy/cdr/sutanbai/ruffle_jp/ruffle.js';
+  document.head.appendChild(s);
+})();
 
